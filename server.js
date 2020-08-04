f302a7d31bdda5025a6b4c6bd4f0cb7039680116
asharma1398 <asharma1398@gmail.com>
2020-08-04 14:12:11 -0400

    new connection

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
+require("dotenv").config;
 
 const PORT = 3000;
 
@@ -15,11 +16,10 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://asharma:qpzm1398@ds259119.mlab.com:59119/heroku_bgzc68hd", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
-
 
 // routes
 app.use(require("./routes/api.js"));
